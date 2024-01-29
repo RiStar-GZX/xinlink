abc177cab876579d70a06f8d7ac4855332c0e87c
XIN <78128788+RiStar-GZX@users.noreply.github.com>
2024-01-29 21:09:07 +0800

    Add files via upload

---
+++ dsp/interface.cpp
@@ -0,0 +1,11 @@
+ #include <interface.h>
+
+XLll info_queue;
+
+uint interface_init(){
+
+}
+
+/*PLU_info get_info(){
+
+}*/
---
+++ dsp/interface.h
@@ -0,0 +1,35 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+//#include <core.h>
+#include <ll.h>
+/*
+enum device_mark_state{
+    DEVICE_MARK_STATE_NONE=0,
+    DEVICE_MARK_STATE_CONNECT,
+    DEVICE_MARK_STATE_ABILIABLE
+};
+
+typedef struct PLU_device_mark{
+    char name[NAME_LENGTH];
+    uint state;
+}PLU_device_mark;
+
+typedef struct PLU_ins_arg{
+    uint type;
+    char name[NAME_LENGTH];
+}PLU_ins_arg;
+
+typedef struct PLU_detail_arg{
+    char op_code[NAME_LENGTH];
+    XLll par;    //PLU_ins_arg
+    XLll device_mark;
+}PLU_detial_arg;
+
+typedef struct PLU_info{
+    XLll link_ll;       //PLU_device_arg
+    XLll detail_arg;    //PLU_detail_arg
+}PLU_info;
+*/
+#endif
+
